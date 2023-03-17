1264a04ef856c6b67c31a3a844425ae71e359c33
Thomas Massey <thomassey476@outlook.com>
2023-03-17 23:39:58 +0000

    Testing

--- .github/workflows/Challenges/Basic_Programs/Maximum_of_two_numbers_in_Python/Maximum_of_two_numbers_in_Python.py
+++ .github/workflows/Challenges/Basic_Programs/Maximum_of_two_numbers_in_Python/Maximum_of_two_numbers_in_Python.py
@@ -1,2 +1,11 @@
-def maximum(a, b):
-    # Complete the function
+import sys
+
+sys.path.append("./Challenges/Basic_Programs/Maximum_of_two_numbers_in_Python/")
+
+from Maximum_of_two_numbers_in_Python import maximum
+
+def test_maximum():
+    assert maximum(7, 8) == 8
+    assert maximum(1, 2) == 2
+    assert maximum(1324154, 123) == 1324154
+    assert maximum(0, 0) == 0

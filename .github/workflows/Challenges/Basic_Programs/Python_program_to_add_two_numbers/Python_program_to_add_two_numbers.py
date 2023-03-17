4d13aeac69875625c75d330a6a9a510d865f595e
Thomas Massey <thomassey476@outlook.com>
2023-03-17 23:08:17 +0000

    Core

--- .github/workflows/Challenges/Basic_Programs/Python_program_to_add_two_numbers/Python_program_to_add_two_numbers.py
+++ .github/workflows/Challenges/Basic_Programs/Python_program_to_add_two_numbers/Python_program_to_add_two_numbers.py
@@ -1,8 +1,8 @@
 import sys
 
-sys.path.append("../../../..")
+sys.path.append("../../../../Challenges/Basic_Programs/Python_program_to_add_two_numbers/")
 
-from Challenges.Basic_Programs.Python_program_to_add_two_numbers.Python_program_to_add_two_numbers import add
+from Python_program_to_add_two_numbers import add
 
 def test_add():
     assert add(7, 8) == 15

7f3307e274d305de689a70fd0489b77503257bdc
Thomas Massey <thomassey476@outlook.com>
2023-03-17 23:05:52 +0000

    verifier

--- .github/workflows/Challenges/Basic_Programs/Python_program_to_add_two_numbers/Python_program_to_add_two_numbers.py
+++ .github/workflows/Challenges/Basic_Programs/Python_program_to_add_two_numbers/Python_program_to_add_two_numbers.py
@@ -1,6 +1,8 @@
 import sys
 
-print(sys.path) 
+sys.path.append("../../../..")
+
+from Challenges.Basic_Programs.Python_program_to_add_two_numbers.Python_program_to_add_two_numbers import add
 
 def test_add():
     assert add(7, 8) == 15
